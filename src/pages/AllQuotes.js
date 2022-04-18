d1431572f3e9fb440ffd204bdf3e0fa70f44bddb
YevaMV <eva280988@gmail.com>
2022-04-18 17:34:55 +0400

    added Dummy Data and form for new quote

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,20 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Dalai Lama',
+    text: 'The purpose of our lives is to be happy.',
+  },
+  {
+    id: 'q2',
+    author: 'John Lennon',
+    text: "Life is what happens when you're busy making other plans.",
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
